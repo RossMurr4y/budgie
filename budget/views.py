4dab20863864af70c1226ce38f043fbe9e897700
Ross Murray <ross.murray@protonmail.com>
2019-12-08 12:28:24 +1100

    created homepage.

--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+def index(request):
+    """The home page for Budgie Budget"""
+    return render(request, 'index.html')
--- superlists/settings.py
+++ superlists/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # budgie apps
+    'budget',
+
+    #default django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
