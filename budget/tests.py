85f84ad75594db39d4dcb5e9cde43d781f6846a8
Ross Murray <ross.murray@protonmail.com>
2019-12-08 13:37:32 +1100

    refactored HomePageTest to use a template and django test client.

--- budget/tests.py
+++ budget/tests.py
@@ -1,19 +1,9 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
 from budget.views import index
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, index)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = index(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>Budgie Budget</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
