29913d7f6a4e448132032b436beef03102579e56
Ross Murray <ross.murray@protonmail.com>
2019-12-08 11:31:14 +1100

    updated to include baseline test comments for rossmurr4y/budgie#1

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_budget_and_calculate_tax(self):
+
+        # Bonnie the Budgerigar has heard about an awesome new budgeting app.
+        # She goes online to check out it's homepage.
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header indicate that she is on the
+        # Budgie Budget Site.
+        self.assertIn('Budgie Budget', self.browser.title)
+
+        # She is invited to add her salary straight away
+
+        # Testing out her desired salary of $100,000, she types it in to the
+        # text field and presses ENTER.
+
+        # Bonnie notices that the page now lists her Salary
+        # alongside the amount of Tax that Bonnie can expect to pay
+
+        # Shocked by the amount of Tax on that Salary, Bonnie enters a lower 
+        # salary of $80,000 and presses ENTER.
+
+        # Bonnie notices that both the Salary and Tax displayed have updated
+        # to reflect her lower salary.
